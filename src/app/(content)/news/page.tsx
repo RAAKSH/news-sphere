5247742bc67390a7314a99b13986d86f353a3935
Raksha <rakshac29@gmail.com>
2025-06-25 10:56:46 +0530

    changes

--- src/app/(content)/news/page.tsx
+++ src/app/(content)/news/page.tsx
@@ -1,36 +1,41 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-"use client";
-import { useEffect, useState } from "react";
+// /* eslint-disable react-hooks/rules-of-hooks */
+// "use client";
+// import { useEffect, useState } from "react";
 import NewsList from "../components/NewsList";
 
-export default function newsPage() {
-  const [news, setNews] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+export default async function newsPage() {
+  const data = await fetch("http://localhost:8080/news");
+  const news = await data.json();
+  // if(!news.ok){
+  //   throw new Error("Error has occured  ");
+  // }
+  // const [news, setNews] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState("");
 
-  useEffect(() => {
-    fetchNews();
-  }, []);
+  // useEffect(() => {
+  //   fetchNews();
+  // }, []);
 
-  const fetchNews = async () => {
-    const data = await fetch("http://localhost:8080/news");
-    if (!data.ok) {
-      setError("Failed to fetch news.");
-      setLoading(true);
-    }
-    const dataJson = await data.json();
-    setLoading(false);
-    setNews(dataJson);
-  };
-  let newsContent;
-  if (loading) {
-    newsContent = <p>Loading....</p>;
-    return newsContent;
-  }
-  if (error) {
-    newsContent = <p>{error}</p>;
-    return <p></p>;
-  }
+  // const fetchNews = async () => {
+   
+  //   if (!data.ok) {
+  //     setError("Failed to fetch news.");
+  //     setLoading(true);
+  //   }
+   
+  //   setLoading(false);
+  //   setNews(dataJson);
+  // };
+   let newsContent;
+  // if (loading) {
+  //   newsContent = <p>Loading....</p>;
+  //   return newsContent;
+  // }
+  // if (error) {
+  //   newsContent = <p>{error}</p>;
+  //   return <p></p>;
+  // }
 
   if (news) {
     newsContent = <NewsList news={news} />;
